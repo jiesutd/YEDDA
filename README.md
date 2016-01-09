6fb6586d807b6cff8b279f844635c0370ffd5fd8
Jiesutd <jie_yang@mymail.sutd.edu.sg>
2016-01-09 20:45:52 +0800

    init version

---
+++ README.md
@@ -0,0 +1,32 @@
+AnnTool: A high efficient corpus annotate tool with GUI.
+======
+
+About:
+====
+This GUI annotation tool is developed with tkinter package in Python. 
+System required: Python 2.7
+
+
+How to use?
+====
+Just run the .py file.
+* Set your shortcut map in the right side of annotation interface, you can set other labels empty if the shortcut number is enough. For example: a == ACTION, c == CONT
+* Click the ***"Update map"*** button to store the map setting
+* Click ***"Open"*** button and select your annotate file. (You may set your file name ended with .txt if possible)
+
+This tool supports two ways of annotation:
+* Select the text and press the corresponding shortcut (i.e. `a` for label ***"Action"***).
+* Type the code at command line (at the bottom of the interface). For example, type `2a3c1a` end with `<Enter>`, it will annotate the following `2` character as type ***'a'(ACTION)***, the following `3` character as type ***'c'(CONT)***, then the following `1` character as  ***'a'(ACTION)***.
+
+The annotated results will be stored synchronously. Annotated file is located at the same directory with origin file with the name of origin name + .ann
+
+
+Other features:
+=====
+* Type `ctrl + z` will undo  the most recent modification
+* Selected the annotated text, such as "[美国＃CONT*]", then press `q`, the annotated text will be recoverd to unannotate format (i.e. "美国").
+
+
+Updating...
+====
+* 2016-01-09: init version
---
+++ anntool.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+##===================================================================##
+#   Annotation tools
+#   Written by Jie Yang at SUTD
+#   Jan. 6, 2016
+# 
+##===================================================================##
+
+from Tkinter import *
+from ttk import *#Frame, Button, Label, Style, Scrollbar
+import tkFileDialog
+import tkFont
+import re
+from collections import deque
+
+
+
+class Example(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        self.fileName = ""
+        self.history = deque(maxlen=10)
+        self.currentContent = deque(maxlen=1)
+        self.pressCommand = {'t':"TITLE", 'o':"ORG", 'u':"UNIV", 'd':"DATE",'a':"ACTION", 'e':"EDU",
+        'p':"PRO", 'g':"GEND",'c':"CONT", 'r':"RACE",'l':"LOC", 's':"SEP", 'n':"NAME"}
+        self.allKey = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.controlCommand = {'q':"unTag", 'ctrl+z':'undo'}
+        self.labelEntryList = []
+        self.shortcutLabelList = []
+        # default GUI display parameter
+        self.textRow = 18
+        self.textColumn = 5
+
+        self.initUI()
+        
+        
+    def initUI(self):
+      
+        self.parent.title("SUTDNLP Annotation Tool")
+        self.pack(fill=BOTH, expand=True)
+        
+        for idx in range(0,self.textColumn):
+            self.columnconfigure(idx, weight =2)
+        # self.columnconfigure(0, weight=2)
+        self.columnconfigure(self.textColumn+2, weight=1)
+        self.columnconfigure(self.textColumn+4, weight=1)
+        for idx in range(0,16):
+            self.rowconfigure(idx, weight =1)
+        # self.rowconfigure(5, weight=1)
+        # self.rowconfigure(5, pad=7)
+        
+        self.lbl = Label(self, text="File: no file is opened")
+        self.lbl.grid(sticky=W, pady=4, padx=5)
+
+        fnt = tkFont.Font(family="Helvetica",size=self.textRow,weight="bold",underline=0)
+        self.text = Text(self, font=fnt,selectbackground='red')
+        self.text.grid(row=1, column=0, columnspan=self.textColumn, rowspan=self.textRow, padx=12, sticky=E+W+S+N)
+
+        self.text.see(1000.0)
+        self.sb = Scrollbar(self)
+        self.sb.grid(row = 1, column = self.textColumn, rowspan = self.textRow, padx=0, sticky = E+W+S+N)
+        self.text['yscrollcommand'] = self.sb.set 
+        self.sb['command'] = self.text.yview 
+        # self.sb.pack()
+
+        abtn = Button(self, text="Open", command=self.onOpen)
+        abtn.grid(row=1, column=self.textColumn +1)
+
+        ubtn = Button(self, text="Update map", command=self.renewPressCommand)
+        ubtn.grid(row=2, column=self.textColumn +1, pady=4)
+
+        cbtn = Button(self, text="Quit", command=self.quit)
+        cbtn.grid(row=3, column=self.textColumn +1, pady=4)
+
+        
+        
+        # obtn = Button(self, text="OK")
+        # obtn.grid(row=5, column=3) 
+
+        lbl_entry = Label(self, text="Command:")
+        lbl_entry.grid(row = self.textRow +1,  sticky = E+W+S+N, pady=4,padx=4)
+        self.entry = Entry(self)
+        self.entry.grid(row = self.textRow +1, columnspan=self.textColumn + 1, rowspan = 1, sticky = E+W+S+N, pady=4, padx=80)
+        self.entry.bind('<Return>', self.returnEnter)
+
+        
+        # for press_key in self.pressCommand.keys():
+        for idx in range(0, len(self.allKey)):
+            press_key = self.allKey[idx:idx+1]
+            self.text.bind(press_key, lambda event, arg=press_key:self.textReturnEnter(event,arg))
+            simplePressKey = "<KeyRelease-" + press_key + ">"
+            self.text.bind(simplePressKey, self.deleteTextInput)
+            controlPlusKey = "<Control-Key-" + press_key + ">"
+            self.text.bind(controlPlusKey, self.keepCurrent)
+            
+        # self.text.bind('<Return>', self.pushToHistoryEvent)
+        # self.text.bind('<KeyRelease-Return>', self.backToHistory)
+
+        self.text.bind('<Control-Key-z>', self.backToHistory)
+
+
+        self.setMapShow()
+
+
+        # control_text = "Control key: \n"
+        # for press_key in self.controlCommand.keys():
+        #     control_text += press_key + "-> " + self.controlCommand[press_key] + '\n'
+        # control_table = Label(self, text=control_text, foreground="red", font=("Helvetica", 13))
+        # control_table.grid(row=4, column=self.textColumn +1)
+
+        self.enter = Button(self, text="Enter", command=self.returnButton)
+        self.enter.grid(row=self.textRow +1, column=self.textColumn +1) 
+ 
+
+    def onOpen(self):
+        ftypes = [('all files', '.*'), ('text files', '.txt'), ('ann files', '.ann')]
+        dlg = tkFileDialog.Open(self, filetypes = ftypes)
+        # file_opt = options =  {}
+        # options['filetypes'] = [('all files', '.*'), ('text files', '.txt')]
+        # dlg = tkFileDialog.askopenfilename(**options)
+        fl = dlg.show()
+        if fl != '':
+            self.text.delete("1.0",END)
+            text = self.readFile(fl)
+            self.text.insert(END, text)
+            self.setNameLabel("File: " + fl)
+            self.setDisplay()
+            self.text.mark_set(INSERT, "1.0")
+
+    def readFile(self, filename):
+        f = open(filename, "r")
+        text = f.read()
+        self.fileName = filename
+        return text
+
+    def setFont(self, value):
+        _family="Helvetica"
+        _size = value
+        _weight="bold"
+        _underline=0
+        fnt = tkFont.Font(family= _family,size= _size,weight= _weight,underline= _underline)
+        Text(self, font=fnt)
+    
+    def setNameLabel(self, new_file):
+        self.lbl.config(text=new_file)
+
+    def returnButton(self):
+        self.pushToHistory()
+        self.returnEnter(event)
+        content = self.entry.get()
+        self.clearCommand()
+        self.executeEntryCommand(content)
+        return content
+
+    def returnEnter(self,event):
+        self.pushToHistory()
+        content = self.entry.get()
+        self.clearCommand()
+        self.executeEntryCommand(content)
+        return content
+
+    def textReturnEnter(self,event, press_key):
+        self.pushToHistory()
+        # print "event: ", press_key
+        # content = self.text.get()
+        self.clearCommand()
+        self.executeCursorCommand(press_key.lower())
+        # self.deleteTextInput()
+        return press_key
+
+    def backToHistory(self,event):
+        if len(self.history) > 0:
+            historyCondition = self.history.pop()
+            # print "history condition: ", historyCondition
+            historyContent = historyCondition[0]
+            # print "history content: ", historyContent
+            cursorIndex = historyCondition[1]
+            # print "get history cursor: ", cursorIndex
+            self.writeFile(self.fileName, historyContent, cursorIndex)
+        else:
+            print "History is empty!"
+        self.text.insert(INSERT, 'p')   # add a word as pad for key release delete
+
+    def keepCurrent(self, event):
+        self.text.insert(INSERT, 'p')
+
+
+    def clearCommand(self):
+        self.entry.delete(0, 'end')
+
+
+    def getText(self):
+        textContent = self.text.get("1.0","end-1c")
+        textContent = textContent.encode('utf-8')
+        return textContent
+
+    def executeCursorCommand(self,command):
+        content = self.getText()
+        firstSelection_index = self.text.index(SEL_FIRST)
+        cursor_index = self.text.index(SEL_LAST)
+        aboveHalf_content = self.text.get('1.0',firstSelection_index).encode('utf-8')
+        followHalf_content = self.text.get(firstSelection_index, "end-1c").encode('utf-8')
+        try:
+            selected_string = self.text.selection_get().encode('utf-8')
+            if ((command == "q")&(selected_string[0] == '[')&(selected_string[-1] == ']')&(selected_string.find('#')>0)&(selected_string.find('*')>0)):
+                print "q yes"
+                if True:    
+                    new_string_list = selected_string.strip('[]').rsplit('#',1)
+                    new_string = ''
+                    # for idx in range(0, len(new_string_list)-1):
+                    #     new_string += new_string_list[idx]
+                    new_string = new_string_list[0]
+                    followHalf_content = followHalf_content.replace(selected_string, new_string,1)
+                    content = aboveHalf_content + followHalf_content
+                    # newcursor_index = "%s - %sc" % (cursor_index, str(len(selected_string)-len(new_string)))
+                    print "q length: ", len(new_string_list[1]), new_string_list[1], cursor_index
+
+                    newcursor_index = "%s - %sc" % (cursor_index, str(len(new_string_list[1])+3))
+                    print "new index: ", newcursor_index
+                    self.writeFile(self.fileName, content, newcursor_index)
+            else:
+                if len(selected_string) > 0:
+                    # print "insert index: ", self.text.index(INSERT) 
+                    followHalf_content, newcursor_index = self.replaceString(followHalf_content, selected_string, command, cursor_index)
+                    content = aboveHalf_content + followHalf_content
+                self.writeFile(self.fileName, content, newcursor_index)
+        except ValueError:
+            print "Have not selected content!"
+
+
+    def executeEntryCommand(self,command):
+        command_list = decompositCommand(command)
+        for idx in range(0, len(command_list)):
+            command = command_list[idx]
+            if len(command) == 2:
+                select_num = int(command[0])
+                command = command[1]
+                content = self.getText()
+                cursor_index = self.text.index(INSERT)
+                # newcursor_index = cursor_index.split('.')[0]+"."+str(int(cursor_index.split('.')[1])+select_num)
+                # print "new cursor position: ", select_num, " with ", newcursor_index, "with ", newcursor_index
+                selected_string = self.text.get(cursor_index, newcursor_index).encode('utf-8')
+                    
+                aboveHalf_content = self.text.get('1.0',cursor_index).encode('utf-8')
+                followHalf_content = self.text.get(cursor_index, "end-1c").encode('utf-8')
+                    
+                if command in self.pressCommand:
+                    if len(selected_string) > 0:
+                        # print "insert index: ", self.text.index(INSERT) 
+                        followHalf_content, newcursor_index = self.replaceString(followHalf_content, selected_string, command, cursor_index)
+                        content = aboveHalf_content + followHalf_content
+                self.writeFile(self.fileName, content, newcursor_index)
+            
+
+    def deleteTextInput(self,event):
+        get_insert = self.text.index(INSERT)
+        insert_list = get_insert.split('.')
+        last_insert = insert_list[0] + "." + str(int(insert_list[1])-1)
+        get_input = self.text.get(last_insert, get_insert).encode('utf-8')
+        # print "get_input: ", get_input
+        aboveHalf_content = self.text.get('1.0',last_insert).encode('utf-8')
+        followHalf_content = self.text.get(last_insert, "end-1c").encode('utf-8')
+        if len(get_input) > 0: 
+            followHalf_content = followHalf_content.replace(get_input, '', 1)
+            content = aboveHalf_content + followHalf_content
+        self.writeFile(self.fileName, content, last_insert)
+
+
+
+    def replaceString(self, content, string, replaceType, cursor_index):
+        if replaceType in self.pressCommand:
+            new_string = "[" + string + '#' + self.pressCommand[replaceType] + "*]" 
+            cursor_indexList = cursor_index.split('.') 
+            newcursor_index = "%s + %sc" % (cursor_index, str(len(self.pressCommand[replaceType])+4))
+            # newcursor_index = cursor_indexList[0] + "." + str(int(cursor_indexList[1])+ len(new_string))
+        else:
+            print "Invaild command!"  
+            print "cursor index: ", self.text.index(INSERT)  
+            return content, cursor_index
+        # print "new string: ", new_string
+        # print "find: ", content.find(string)
+        content = content.replace(string, new_string,1)
+        # print "content: ", content
+        return content, newcursor_index
+
+
+    def writeFile(self, fileName, content, newcursor_index):
+        if len(fileName) > 0:
+            if ".ann" in fileName:
+                new_name = fileName
+                ann_file = open(new_name, 'w')
+                ann_file.write(content)
+                ann_file.close()
+            else:
+                new_name = fileName+'.ann'
+                ann_file = open(new_name, 'w')
+                ann_file.write(content)
+                ann_file.close()   
+            # print "Writed to new file: ", new_name 
+            self.autoLoadNewFile(new_name, newcursor_index)
+        else:
+            print "Don't write to empty file!"        
+
+
+    def autoLoadNewFile(self, fileName, newcursor_index):
+        if len(fileName) > 0:
+            self.text.delete("1.0",END)
+            text = self.readFile(fileName)
+            self.text.insert("end-1c", text)
+            self.setNameLabel("File: " + fileName)
+            self.setDisplay()
+            self.text.mark_set(INSERT, newcursor_index)
+            
+            
+    def setDisplay(self):
+        self.text.config(insertbackground='red', insertwidth=4)
+
+        self.text.mark_set("matchStart", "1.0")
+        self.text.mark_set("matchEnd", "1.0") 
+        self.text.mark_set("searchLimit", 'end-1c')
+
+        
+        countVar = StringVar()
+        # for annotate_type in self.pressCommand.values():
+        while True:
+            self.text.tag_configure("catagory", background="green")
+            self.text.tag_configure("edge", background="blue")
+                # type_pattern = re.compile(r'\[*\]')
+                # type_pattern = '[\*' + "\?" + '#' + annotate_type + '*]'
+                # print "annotate type: ", annotate_type
+            pos = self.text.search(r'\[.*?\#.*?\*\]', "matchEnd" , "searchLimit",  count=countVar, regexp=True)
+            if pos =="":
+                break
+            self.text.mark_set("matchStart", pos)
+            self.text.mark_set("matchEnd", "%s+%sc" % (pos, countVar.get()))
+            
+            first_pos = pos
+            second_pos = "%s+%sc" % (pos, str(1))
+            lastsecond_pos = "%s+%sc" % (pos, str(int(countVar.get())-1))
+            last_pos = "%s + %sc" %(pos, countVar.get())
+
+            self.text.tag_add("catagory", second_pos, lastsecond_pos)
+            self.text.tag_add("edge", first_pos, second_pos)
+            self.text.tag_add("edge", lastsecond_pos, last_pos)
+            
+    
+    def pushToHistory(self):
+        currentList = []
+        content = self.getText()
+        cursorPosition = self.text.index(INSERT)
+        # print "push to history cursor: ", cursorPosition
+        currentList.append(content)
+        currentList.append(cursorPosition)
+        self.history.append(currentList)
+
+    def pushToHistoryEvent(self,event):
+        currentList = []
+        content = self.getText()
+        cursorPosition = self.text.index(INSERT)
+        # print "push to history cursor: ", cursorPosition
+        currentList.append(content)
+        currentList.append(cursorPosition)
+        self.history.append(currentList)
+
+
+    def renewPressCommand(self):
+        seq = 0
+        new_dict = {}
+        listLength = len(self.labelEntryList)
+        delete_num = 0
+        for key in sorted(self.pressCommand):
+            label = self.labelEntryList[seq].get()
+            if len(label) > 0:
+                new_dict[key] = label
+            else: 
+                delete_num += 1
+            seq += 1
+        self.pressCommand = new_dict
+        for idx in range(1, delete_num+1):
+            self.labelEntryList[listLength-idx].delete(0,END)
+            self.shortcutLabelList[listLength-idx].config(text="NON== ") 
+
+        self.setMapShow()
+
+
+    def setMapShow(self):
+        hight = len(self.pressCommand)
+        width = 2
+        row = 0
+        mapLabel = Label(self, text ="Shortcuts map Labels", foreground="blue", font=("Helvetica", 14, "bold"))
+        mapLabel.grid(row=0, column = self.textColumn +2,columnspan=2, rowspan = 1, padx = 10)
+        self.labelEntryList = []
+        self.shortcutLabelList = []
+        for key in sorted(self.pressCommand):
+            row += 1
+            # print "key: ", key, "  command: ", self.pressCommand[key]
+            symbolLabel = Label(self, text =key + " == ", foreground="blue", font=("Helvetica", 14, "bold"))
+            symbolLabel.grid(row=row, column = self.textColumn +2,columnspan=1, rowspan = 1, padx = 3)
+            self.shortcutLabelList.append(symbolLabel)
+
+            labelEntry = Entry(self, foreground="blue", font=("Helvetica", 14, "bold"))
+            labelEntry.insert(0, self.pressCommand[key])
+            labelEntry.grid(row=row, column = self.textColumn +3, columnspan=1, rowspan = 1)
+            self.labelEntryList.append(labelEntry)
+            # print "row: ", row
+
+
+
+def decompositCommand(command_string):
+    command_list = []
+    each_command = []
+    num_select = ''
+    for idx in range(0, len(command_string)):
+        if command_string[idx].isdigit():
+            num_select += command_string[idx]
+        else:
+            each_command.append(num_select)
+            each_command.append(command_string[idx])
+            command_list.append(each_command)
+            each_command = []
+            num_select =''
+    # print command_list
+    return command_list
+
+
+
+def main():
+    root = Tk()
+    root.geometry("1000x700+200+200")
+    app = Example(root)
+    app.setFont(17)
+    
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()
+
+
+
